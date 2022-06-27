3f6cb8a5c9deabcf18d84c5d54e502b97b4197de
hwchoi <bluster8093@naver.com>
2022-06-27 22:55:36 +0900

    boj 24480

---
+++ 24480.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(1000000)
+
+def DFS(v):
+	global cnt
+	# order[cnt] = v로 작성하고 아래에서 거치지 않는 노드를 따로 처리해 주어야 한다고 생각함
+	# https://hueco.tistory.com/196 참고함
+	order[v] = cnt
+	cnt += 1
+	visited[v] = True
+	for i in graph[v]:
+		if not visited[i]:
+			DFS(i)
+
+n, m, r = map(int, input().split())
+
+graph = [[] for _ in range(n + 1)]
+for _ in range(m):
+	x, y = map(int, input().split())
+	graph[x].append(y)
+	graph[y].append(x)
+
+for srt in graph:
+	# 24479에서 아래와 같이 정렬 순서만 역순으로 바꿈
+	srt.sort(reverse = True)
+
+visited = [False] * (n + 1)
+order = [0] * (n + 1)
+cnt = 1
+DFS(r)
+
+[print(order[i]) for i in range(1, n + 1)]
